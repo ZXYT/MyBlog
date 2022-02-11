f0cacdd8e0b75fb644380a74ccb3432e1b0a9ee7
ZT <2417407179@qq.com>
2022-02-11 09:37:47 +0800

    feat: 添加favicon

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -7,11 +7,22 @@ export default defineUserConfig<DefaultThemeOptions>({
   title: "ZT'Blog",
   description: "This is ZT'Blog",
   theme: "@vuepress/theme-default",
+  head: [
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/assets/logo.png",
+      },
+    ],
+  ],
   themeConfig: {
-    logo: "https://vuejs.org/images/logo.png",
+    logo: "/assets/logo.svg",
     navbar: [
       "/",
-      { text: "文章", link: "/blog" },
+      { text: "笔记", link: "/blog" },
       { text: "项目", link: "/project" },
     ],
     repo: "https://github.com/ZXYT/myblog",
